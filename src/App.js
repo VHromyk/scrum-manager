12d81d7c412f6111111c5aa8c6a38bb424e8f87d
Vitalii Hromyk <hromykk@gmail.com>
2021-06-27 17:14:01 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import AddButton from './components/AddButton';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <AddButton />
     </div>
   );

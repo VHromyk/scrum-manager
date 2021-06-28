035ec0537e1ec7afd243612e94fe5016931e54b3
Vitalii Hromyk <65778221+VHromyk@users.noreply.github.com>
2021-06-29 00:06:22 +0300

    Delete src/Components directory
    added folder

--- src/Components/HeaderWrapper/HeaderWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './HeaderWrapper.module.scss';
-
-const HeaderWrapper = ({ children }) => (
-  <div className={styles.wrapper}>{children}</div>
-);
-
-export default HeaderWrapper;

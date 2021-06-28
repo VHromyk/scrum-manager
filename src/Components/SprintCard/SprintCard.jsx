75abe2fcb59ddf783037590efad16b48b2e90f9e
NataliiaLazorenko <natalystruk@gmail.com>
2021-06-28 11:48:45 +0300

    Edited SprintCard

--- src/Components/SprintCard/SprintCard.jsx
+++ src/Components/SprintCard/SprintCard.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import IconButton from '../IconButton';
 import SvgComponent from '../SvgComponent';
 import styles from './SprintCard.module.scss';
 
-const SprintCard = ({ id, sprintName, startDate, endDate, duration, handleDeleteSprint }) => (
-  <li key={id} className={SprintCard}>
+const SprintCard = ({
+  id,
+  sprintName,
+  startDate,
+  endDate,
+  duration,
+  handleDeleteSprint,
+}) => (
+  <li key={id} className={styles.SprintCard}>
     <p className={styles.SprintTitle}>{sprintName}</p>
     <ul className={styles.SprintCardList}>
-      <li className={styles.SprintItem}><span>Start date</span><span>{startDate}</span></li>
-      <li className={styles.SprintItem}><span>End date</span><span>{endDate}</span></li>
-      <li className={styles.SprintItem}><span>Duration</span><span>{duration}</span></li>
+      <li className={styles.SprintItem}>
+        <span>Start date</span>
+        <span>{startDate}</span>
+      </li>
+      <li className={styles.SprintItem}>
+        <span>End date</span>
+        <span>{endDate}</span>
+      </li>
+      <li className={styles.SprintItem}>
+        <span>Duration</span>
+        <span>{duration}</span>
+      </li>
     </ul>
-    <button className={styles.Btn} onClick={() => handleDeleteSprint(id)}><SvgComponent name='delete-grey' width='20px' height='20px' /></button>
+    <IconButton
+      classes={styles.deleteSprintBtn}
+      onClick={() => handleDeleteSprint(id)}
+      aria-label="delete sprint button"
+    >
+      <SvgComponent name="delete" classes={styles.deleteSprintIcon} />
+    </IconButton>
   </li>
-)
+);
 
 SprintCard.propTypes = {
   id: PropTypes.string.isRequired,

57601167a7804d50a7afaf9b8b36a907ff4814af
NataliiaLazorenko <natalystruk@gmail.com>
2021-06-30 00:52:27 +0300

    Added UserMenu rendering when user is logged in

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Container.module.scss';
 
-const Container = ({ children }) => (
-  <div className={styles.container}>{children}</div>
-);
+const Container = ({ children, classes }) => {
+  const containerClasses = [styles.container];
+
+  if (classes) {
+    containerClasses.push(classes);
+  }
+
+  return <div className={containerClasses.join(' ')}>{children}</div>;
+};
 
 export default Container;
+
+Container.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.string,
+};
+
+Container.defaultProps = {
+  classes: '',
+};

1d87e20969af3c06557e1c5e84cc69ebd106beb2
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-06-30 19:22:12 +0300

    add users to proj form

---
+++ src/components/AddPeopleList/AddPeopleList.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import styles from './AddPeopleList.module.scss';
+
+const users = [
+    { id: 'id-1', email: 'vitaly@gmail.com' },
+    { id: 'id-2', email: 'lena@gmail.com' },
+    { id: 'id-3', email: 'nataly@gmail.com' },
+    { id: 'id-4', email: 'halyna@gmail.com' },
+    { id: 'id-5', email: 'dima@gmail.com' },
+]
+
+// const users = [];
+
+const AddPeopleList = () => {
+    return (
+        <ul className={styles.addPeopleList}>
+            {users.map(user => (
+                <li
+                    key={user.id}
+                    className={styles.addPeopleListItem}
+                >
+                    {user.email}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default AddPeopleList;
+
+AddPeopleList.propTypes = {
+    users: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            email: PropTypes.string.isRequired,
+        }),
+    ),
+};

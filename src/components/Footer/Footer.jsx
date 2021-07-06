010238e1d740e4dd7bd941d5c70a4a3faf79966d
Olena <stopinahelen@gmail.com>
2021-07-06 23:11:19 +0300

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './Footer.module.scss';
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p>&#169; 2021 | All Rights Reserved | Developed with by GoIT Students</p>
+    </div>
+  );
+};
+
+export default Footer;

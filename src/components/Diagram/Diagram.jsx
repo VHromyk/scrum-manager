70a21b58fda759d1df3e84fa2d09c1439bd6855e
Vitalii Hromyk <hromykk@gmail.com>
2021-07-08 16:01:23 +0300

    fix commit

--- src/components/Diagram/Diagram.jsx
+++ src/components/Diagram/Diagram.jsx
@@ -1,16 +1,38 @@
+import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import tasksSelectors from '../../redux/tasks/tasks-selectors';
+import sprintsSelectors from '../../redux/sprints/sprints-selectors';
 import styles from './Diagram.module.css';
 import { Line } from 'react-chartjs-2';
 import _ from 'lodash';
 
 function Diagram() {
   const getAll = useSelector(tasksSelectors.getTasks);
+  const getSprints = useSelector(sprintsSelectors.getAllSprints);
+
+  const { sprintId } = useParams();
+  console.log();
+
+  console.log('Взять все таски:', getAll);
+  //Массив всіх тасок
+
+  // console.log('Взять все спринты:', getSprints);
+
+  const getIdTask = getAll[0].mainSprint.id;
+  console.log('Id задач:', getIdTask);
+  // получаем Id таски и они для каждого спринта разные
+
+  let countDayForSprints = null;
+
+  if (getSprints.length > 0) {
+    let filterArr = getSprints.filter(item => item.id === getIdTask); //Берем количество дней для каждого спринта
+    countDayForSprints = filterArr[0].duration;
+    console.log('Массив отфильтрированых элементов:', filterArr);
+  }
+
+  console.log('Взять количество дней на спринт:', countDayForSprints);
+
   const months = ['JUL', 'AUG'];
-
-  console.log('Взять все таски:', getAll); //Массив всіх тасок
-
-  const countDayForSprints = 12; //Захардкожена кількість днів спринта
 
   const sumRedLine = getAll.reduce(function (cnt, getAll) {
     return cnt + getAll.scheduledHours;
@@ -20,18 +42,22 @@ function Diagram() {
 
   const DaysRedLine = () => {
     let arr = [];
-
-    for (let i = 0; i <= getAll.length; i += 1) {
-      let sumAllRedLine = sumRedLine / countDayForSprints;
-      let typeToNumber = Math.floor(sumAllRedLine * 100) / 100;
-      console.log(typeToNumber);
-      arr.push(typeToNumber);
+    let firstIndex = sumRedLine; //первый индекс массива 105 сумма всех часов;
+    arr.push(firstIndex); // создаем первый индекс массива;
+    let sumAllRedLine = sumRedLine / countDayForSprints; // делим общее количество запланированых часов на кол-во дней спринта
+    for (let i = 0; i <= countDayForSprints; i += 1) {
+      firstIndex = firstIndex - sumAllRedLine;
+      let typeToNumber = Math.floor(firstIndex * 100) / 100; // обрезаем число до двух знаков после запятой;
+      if (typeToNumber >= 0) {
+        arr.push(typeToNumber);
+        // пока первый индекс больше 0 пушим в массив числа;
+      }
     }
-    console.log(arr);
+    console.log('Массив элементов для красной линии:', arr);
     return arr;
   };
 
-  console.log(DaysRedLine());
+  DaysRedLine();
 
   const DaysBlueLine = () => {
     let arrBlueLine = [];
